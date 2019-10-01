faa789297b353e43e315059e5023001ed2cad669
白鳥雅人 <whitesplash0703@icloud.com>
2019-10-02 06:29:32 +0900

    Initial Commit

---
+++ tableViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewApp
+//
+//  Created by swans on 2019/10/02.
+//  Copyright © 2019 swans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
